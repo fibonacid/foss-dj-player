d7f025dfaf44dc76c4a4778ed329c3b7b21d789e
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-02-21 23:28:54 +0100

    fixup! feat(pi-gen): automount usb to /media/usb/(devname) with udev

--- pi-gen/stage3/02-setup-usb/00-run.sh
+++ pi-gen/stage3/02-setup-usb/00-run.sh
@@ -2,9 +2,11 @@
 
 on_chroot <<-EOF
     # Ensure mount point exists
-    mkdir -p /media/usb/sda1
+    mkdir -p /media/usb1
+    mkdir -p /media/usb2
 
     # Append fstab entry if not already present
     grep -qxF '/dev/sda1  /media/usb/sda1  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0' /etc/fstab || \
-    echo '/dev/sda1  /media/usb/sda1  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0' >> /etc/fstab
+    echo '/dev/sda1  /media/usb1  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0' >> /etc/fstab
+    echo '/dev/sda1  /media/usb2  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0' >> /etc/fstab
 EOF
