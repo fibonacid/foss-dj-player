b7a7c49ec6891bb0d6fc7406981c50f88d96c99c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-02-21 17:22:14 +0100

    fix(pi-gen): typo in chown commands

--- pi-gen/stage3/01-setup-mixxx/01-run.sh
+++ pi-gen/stage3/01-setup-mixxx/01-run.sh
@@ -4,5 +4,5 @@ cp -r ./files/mixxx "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.mixxx"
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Music"
 
 on_chroot <<-EOF
-		chown -r ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+		chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 EOF
