080df932b33c15414a68388b590fadc0c7fac26d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-02-25 18:19:45 +0100

    fixup! run chroot

--- pi-gen/stage3/02-setup-usb/00-run-chroot.sh
+++ pi-gen/stage3/02-setup-usb/00-run-chroot.sh
@@ -7,7 +7,7 @@ mkdir -p /media/usb2
 # Define fstab entries
 entries=(
 	"/dev/sda1  /media/usb1  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0"
-	"/dev/sda1  /media/usb2  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0"
+	"/dev/sda2  /media/usb2  auto  nofail,x-systemd.automount,uid=1000,gid=1000,umask=022  0  0"
 )
 
 # Append each entry if not already present
