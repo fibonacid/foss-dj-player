7972d4d1894f42081e77f5b7aba31cb50bcba10b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-03-14 11:33:37 +0100

    create starlight project

---
+++ docs/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "FOSS DJ PLAYER",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/fibonacid/foss-dj-player",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
