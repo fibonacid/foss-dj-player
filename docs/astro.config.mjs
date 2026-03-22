85694f42665f072288b68bf3b38e850f8f53e4a0
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-03-22 17:37:29 +0100

    docs: create website (#20)
    * move docs to _docs
    
    * create starlight project
    
    * docs: basic splash screen + intro page
    
    * add custom font
    
    * trim down intro
    
    * add logos
    
    * increase line height
    
    * update logo and add discord
    
    * deploy docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,51 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "FOSS DJ PLAYER",
+      logo: {
+        light: "./src/assets/fdjp_light_2xl.png",
+        dark: "./src/assets/fdjp_dark_2xl.png",
+        replacesTitle: true,
+      },
+      customCss: [
+        // Relative path to your @font-face CSS file.
+        "./src/fonts/font-face.css",
+        "./src/styles/custom.css",
+      ],
+      social: [
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/WABteAtQdq",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/fibonacid/foss-dj-player",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Introduction",
+          link: "/intro",
+        },
+        // {
+        //   label: "Guides",
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: "Example Guide", slug: "guides/example" },
+        //   ],
+        // },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+      ],
+    }),
+  ],
+});
