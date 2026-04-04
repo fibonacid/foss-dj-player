570594ba5ac8294dca7df2c68c9a29c1fce92832
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2026-04-04 20:31:26 +0200

    use zsh for pi user

--- alpine/build.sh
+++ alpine/build.sh
@@ -7,7 +7,7 @@ export APK_OPTS="--arch $ARCH"
 ./alpine-make-rootfs \
   --branch latest-stable \
   --keys-dir=./keys \
-  --packages 'linux-rpi curl vim alpine-conf seatd' \
+  --packages 'linux-rpi curl vim alpine-conf seatd zsh' \
   --script-chroot \
   "./deploy/example-$(date +%Y%m%d).tar.gz" -- ./example/install.sh
 
--- alpine/example/install.sh
+++ alpine/example/install.sh
@@ -5,7 +5,7 @@ set -eu
 setup-alpine -c ./setup-alpine.answers
 
 # setup user
-adduser -u 1000 -G wheel -D pi
+adduser -u 1000 -G wheel -s /bin/zsh -D pi
 addgroup pi seat
 addgroup pi wheel
 
